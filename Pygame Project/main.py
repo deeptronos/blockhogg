4c0fde478db4336e09a9d55ee1cefa985ad618ab
deeptronos <ecnemec1132@gmail.com>
2019-03-11 14:44:05 -0500

    updated main.py
    made it make a little window at 640x480

--- Pygame Project/main.py
+++ Pygame Project/main.py
@@ -1,9 +1,9 @@
 import pygame
-pygame.display.init	
 
-
-screenSize = (200, 150)
+pygame.init()
+screenSize = (640, 480)
 screen = pygame.display.set_mode(screenSize)
+done = False	
 
 
 
@@ -13,30 +13,3 @@ while not done:
             done = True
     pygame.display.flip()
 
-
-
-# import sys, pygame
-# pygame.init()
-
-# size = width, height = 320, 240
-# speed = [2, 2]
-# black = 0, 0, 0
-
-# screen = pygame.display.set_mode(size)
-
-# ball = pygame.draw.circle(screen, (255,0,0), (150,150), 15, 2)
-# ballrect = ball.get_rect()
-
-# while 1:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT: sys.exit()
-
-#     ballrect = ballrect.move(speed)
-#     if ballrect.left < 0 or ballrect.right > width:
-#         speed[0] = -speed[0]
-#     if ballrect.top < 0 or ballrect.bottom > height:
-#         speed[1] = -speed[1]
-
-#     screen.fill(black)
-#     screen.blit(ball, ballrect)
-#     pygame.display.flip() 
